d3ef52aca9dcf3ad3b89d02d40c3954a2e754f60
Simon Bugge Siggaard <simsig+githubOfficial@gmail.com>
2016-05-23 10:31:18 +0200

    Initial commit Added some graph generators

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from graph_generators import grid_graph, powerlaw_cluster_graph
+
+__all__ = ['grid_graph, powerlaw_cluster_graph']
---
+++ graph_generators/grid_graph.py
@@ -0,0 +1,19 @@
+import networkx as nx
+
+def grid_graph(dim, scale=1.0, periodic=False):
+    '''
+    Creates a NetworkX grid graph with the original
+    node labels as positions in the 'pos' node attribute.
+    
+    Will position the nodes in a unit square unless scale!=0,
+    in which case the square is scaled accordingly.
+    '''
+    Network = nx.grid_graph(dim=dim, periodic=periodic)
+    positions = Network.nodes()
+    positions = [(scale*i/float(n-1), scale*j/float(n-1))
+                 for (i, j) in positions]
+    Network = nx.convert_node_labels_to_integers(Network0)
+    pos = dict(zip(Network.nodes(), positions))
+    nx.set_node_attributes(Network, 'pos', pos)
+    
+    return Network
