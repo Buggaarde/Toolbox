0273b52b9a0f8dc3545ed7dec7f1ad2a5bb68e61
Simon Bugge Siggaard <simsig+githubOfficial@gmail.com>
2016-07-08 11:40:20 +0200

    Updated init-files

--- __init__.py
+++ __init__.py
@@ -1,2 +1,11 @@
-import toolbox.Alternate_solvers as solvers
-__all__ = ['plotting', 'graph_generators, solvers']
+import platform
+if platform.python_version() == '2.7.9':
+    import toolbox.Alternate_solvers as solvers
+    __all__ = ['plotting',
+               'graph_generators',
+               'decorators',
+               'solvers']
+else:
+    __all__ = ['plotting',
+               'graph_generators',
+               'decorators']
--- graph_generators/__init__.py
+++ graph_generators/__init__.py
@@ -1,4 +1,15 @@
 from .grid_graph import grid_graph
 from .powerlaw_cluster_graph import powerlaw_cluster_graph
+from .gnm_random_graph import gnm_random_graph
+from .scale_free_graph import scale_free_graph
+from .small_world import connected_watts_strogatz_graph
 
-__all__ = ['grid_graph', 'powerlaw_cluster_graph']
+from .graph_io import save_graph, load_graph
+
+__all__ = ['grid_graph',
+           'powerlaw_cluster_graph',
+           'gnm_random_graph',
+           'scale_free_graph',
+           'connected_watts_strogatz_graph',
+           'load_graph',
+           'save_graph']
